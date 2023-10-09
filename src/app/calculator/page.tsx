0a50e1ba8e188dce359915fbc231ce819e4855ca
Victor Micco <victor.oficial093@gmail.com>
2023-10-09 00:42:11 -0300

    root path button

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -3,7 +3,17 @@ import Calculator from "@/components/calculatorPage/Calculator";
 export default function calculator() {
 	return (
 		<>
-			<h1>Calculadora</h1>
+			<div className="flex justify-center pt-6">
+				<h1 className=" font-bold text-4xl flex justify-center pt-6 pr-6">
+					Calculadora
+				</h1>
+				<a
+					className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded flex items-center"
+					href="/"
+				>
+					Voltar
+				</a>
+			</div>
 			<Calculator />
 		</>
 	);
